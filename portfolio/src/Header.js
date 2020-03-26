11dc912116d9646170e479f37c96b4db66b4c79e
Josh Faison <Joshfaiso@yahoo.com>
2020-03-25 20:18:17 -0400

    started navbar

---
+++ portfolio/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+
+function App() {
+  return (
+    <div className="Header">
+      <nav class="navbar navbar-dark bg-dark" style={{ color: "white", width: "100%", padding: "20px", fontSize: "35px" }}>
+        Josh Faison
+      </nav>
+    </div>
+  );
+}
+
+export default App;
